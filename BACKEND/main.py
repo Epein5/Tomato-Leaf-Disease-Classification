4a94e8ad5a8fd879d2fe13e143978cc40c407b24
Epein5 <aayushgtm5@gmail.com>
2024-05-25 17:59:44 +0545

    first deploytmenrr

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -7,7 +7,7 @@ from fastapi.requests import Request
 from typing import List
 import uvicorn
 from io import BytesIO
-from .predict import predict_image
+# from .predict import predict_image
 import os
 import sys
 from .data import disease_data  # Import the disease data
@@ -25,39 +25,39 @@ app = FastAPI()
 #     return {"message": "Welcome to the Image Classification API"}
 
 templates = Jinja2Templates(directory="templates")
-app.mount("/ML", StaticFiles(directory="ML"), name="ML")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/ML", StaticFiles(directory="ML"), name="ML")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "message": "Welcome to the Image Classification API"})
 
-@app.get("/info", response_class=HTMLResponse)
-async def read_info(request: Request):
-    return templates.TemplateResponse("index2.html", {"request": request})
+# @app.get("/info", response_class=HTMLResponse)
+# async def read_info(request: Request):
+#     return templates.TemplateResponse("index2.html", {"request": request})
 
-@app.get("/classify", response_class=HTMLResponse)
-async def read_info(request: Request):
-    return templates.TemplateResponse("index3.html", {"request": request})
+# @app.get("/classify", response_class=HTMLResponse)
+# async def read_info(request: Request):
+#     return templates.TemplateResponse("index3.html", {"request": request})
 
 
 
-@app.post("/classify_image")
-async def classify_image_endpoint(image: UploadFile = File(...)):
-    # Read the image data
-    image_data = await image.read()
+# @app.post("/classify_image")
+# async def classify_image_endpoint(image: UploadFile = File(...)):
+#     # Read the image data
+#     image_data = await image.read()
 
-    # Make predictions
-    predicted_class = predict_image(BytesIO(image_data))
+#     # Make predictions
+#     predicted_class = predict_image(BytesIO(image_data))
 
-    # Fetch disease information
-    disease_info = disease_data.get(predicted_class, {"Description": "Unknown", "Prevention_measures": "Unknown"})
+#     # Fetch disease information
+#     disease_info = disease_data.get(predicted_class, {"Description": "Unknown", "Prevention_measures": "Unknown"})
 
-    # Return the predicted class and disease information
-    return {
-        "predicted_class": predicted_class,
-        "disease_info": disease_info
-    }
+#     # Return the predicted class and disease information
+#     return {
+#         "predicted_class": predicted_class,
+#         "disease_info": disease_info
+#     }
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
